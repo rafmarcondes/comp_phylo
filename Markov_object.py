510d3c06a24d9d07a89e735b11e615a4f92e7da2
Rafael S. Marcondes <rmarco3@lsu.edu>
2015-02-27 15:18:57 -0600

    Update Markov_object.py

--- Markov_object.py
+++ Markov_object.py
@@ -22,60 +22,65 @@ def sdd(events,probs):
     return (random.choice(newlist)) #and finally, i ramdonly sample an element from newlist
     
 
+
+
+
 class MarkovChain(object): #define class. whatever is inside the parentheses tells python what class markov inherits 
 #attributes from. (object) tells that it inherits from nothing
-    def __init__(self, q,states, v): #a constructor. defines the variables i want associated with this 
+    def __init__(self, q, states,v): #a constructor. defines the variables i want associated with this 
     #instance of class
-        self.q=q #a q-matrix
-        self.states=states
-        self.v=v
-       
-    def contmark(states,q,v) : #method to run a simulation and get a chain
-        """IMPORTANT:rows and columns in the q-matrix MUST be in the order ACGT
+        self.q=q #a q matrix
+        self.states=states #the state space
+        self.v=v #a branch length
+      
+        
+    def contmark(self) :        
+        """IMPORTANT:rows and columns in the q-matrix MUST be in the order a,c,g,t
         IMPORTANT2: the q-matrix should be a numpy matrix object, a 'list of lists' won't work
         IMPORTANT3: this function is dependendt on my discrete sampling function, 'sdd', pasted above"""
-        
         import scipy as sp
         import random
-        t=sp.linalg.expm(q*v) #get the transition probability matrix from the q-matrix
+        statup=tuple(self.states) 
         chain=[]
         times=[]
         elapsedtime=0 #initialize a variable for the elapsed time
-        currstate=random.choice(states) #randomly choose the initial state
+        currstate=random.choice(self.states) #randomly choose the initial state
         chain.append(currstate)
         wt=0 #initialize the waiting time variable
-        while elapsedtime<v :
-            """this series of if statements chooses the appropriate lambda (rate of the exponential distribution), given the currente state"""
+        while elapsedtime<self.v :
             if currstate=='a' :
-                lambd=-(q.item(0,0))
+                lambd=-(self.q.item(0,0))
             if currstate=='c' :
-                lambd=-(q.item(1,1))
+                lambd=-(self.q.item(1,1))
             if currstate=='g' :
-                lambd=-(q.item(2,2))
+                lambd=-(self.q.item(2,2))
             if currstate=='t' :
-                lambd=-(q.item(3,3))
+                lambd=-(self.q.item(3,3))
             wt=random.expovariate(lambd) #use the appropriate lambda to draw a waiting time
             times.append(wt)
             elapsedtime+=wt
-            """this series of if/elif statements draws the next state"""
             if currstate=='a' :
-                currstate=sdd(states,t[0])
-            elif currstate=='c':
-                currstate=sdd(states,t[1])
-            elif currstate=='g':
-                currstate=sdd(states,t[2])
-            elif currstate=='t':
-                currstate=sdd(states,t[3])       
-        chain.append(currstate)
-        return chain
+                self.states.remove('a') #in order not to draw the same state as the current one
+                currstate=sdd(self.states,(self.q.item(1),self.q.item(2),self.q.item(3)))
+            elif currstate=='c' :
+                self.states.remove('c')
+                currstate=sdd(self.states,(self.q.item(4),self.q.item(6),self.q.item(7)))
+            elif currstate=='g' :
+                self.states.remove('g')
+                currstate=sdd(self.states,(self.q.item(8),self.q.item(9),self.q.item(11)))
+            elif currstate=='t' :
+                self.states.remove('t')
+                currstate=sdd(self.states,(self.q.item(12),self.q.item(13),self.q.item(14))   )
+            chain.append(currstate)
+            self.states=list(statup) #reset the 'states' list to its original composition
+        return chain, times
+    
 
-        #end of class definition
-        
+""""ok, created the class, now let's try to use it:"""
 
-""""ok, created the class, now ket's try to use it:"""
 import numpy
-mymarkov=MarkovChain (q=numpy.matrix('-1.916 0.541 0.787 0.588; 0.148 -1.069 0.415 0.506; 0.286 0.170 -0.591 0.135; 0.525 0.236 0.594 -1.355'), states=('a','c','g','t'), v=5) #create an instance of MarkovChain class  
+bla=numpy.matrix('-1.916 0.541 0.787 0.588; 0.148 -1.069 0.415 0.506; 0.286 0.170 -0.591 0.135; 0.525 0.236 0.594 -1.355')
+mymarkov=MarkovChain(q=bla, states=['a','c','g','t'], v=5) #create an instance of MarkovChain class  
 
-print mymarkov.q #Try to retrieve matrix variable from object - it works!
-
-print mymarkov.contmark #NOT WORKING
+print mymarkov.q #retrieve matrix variable from object
+print mymarkov.contmark() #use contmark method to obtain a chain
