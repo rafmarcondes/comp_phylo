2b375288655e79a89c96831136b72552195011af
Rafael S. Marcondes <rmarco3@lsu.edu>
2015-02-27 20:48:17 -0600

    Create timeperstate.py
    A function that calculates the times spent in each state in a continuous time markov chain simulation of character evolution in a dna position.

---
+++ timeperstate.py
@@ -0,0 +1,27 @@
+def timeperstate(chainandtimes) :
+    """a function that calculates the times spent in each state, given a markov chain and the waiting times
+    between transition.
+    INPUT: a single argument, which must be a list or tuple in the format (chain,times), in which chain and times
+    also are lists or tuples. This is the format of the output of my contmark function
+    OUTPUT: a list with for itens, each being the total time spent in states a, c, g and t, in that order"""
+    chain=chainandtimes[0]
+    times=chainandtimes[1]
+    ttime=0
+    atime=0
+    ctime=0
+    gtime=0
+    for n in range(0,len(chain)):
+            if chain[n]=='a' :
+                atime+=times[n]
+            if chain[n]=='c' :
+                ctime+=times[n]
+            if chain[n]=='t' :
+                ttime+=times[n]
+            if chain[n]=='g' :
+                gtime+=times[n]
+    actgtimes=[]
+    actgtimes.append(atime)
+    actgtimes.append(ctime)
+    actgtimes.append(gtime)
+    actgtimes.append(ttime)
+    return actgtimes
